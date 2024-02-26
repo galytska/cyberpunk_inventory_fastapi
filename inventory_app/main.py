419f725529fe71e7f7cf32b68434bf6c80310475
FGVMdizT <galytska13@gmail.com>
2024-02-26 12:52:20 +0200

    add auth router

--- inventory_app/main.py
+++ inventory_app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from inventory_app import models
 from inventory_app.database import engine
-from inventory_app.routers.items import router
+from inventory_app.routers import auth, items
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(router)
+
+app.include_router(items.router)
+app.include_router(auth.router)

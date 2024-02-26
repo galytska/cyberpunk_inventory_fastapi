99a810e90a9d9af42973bd36f4c4f17033f2a31e
FGVMdizT <galytska13@gmail.com>
2024-02-26 12:34:23 +0200

    inventory_app/tests/conftest.py

--- inventory_app/main.py
+++ inventory_app/main.py
@@ -1,56 +1,9 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from sqlalchemy.orm import Session
-
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+from fastapi import FastAPI
+from inventory_app import models
+from inventory_app.database import engine
+from inventory_app.routers.items import router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post("/items/", response_model=schemas.Item)
-def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    db_item = crud.get_item_by_name(db, name=item.name)
-    if db_item:
-        raise HTTPException(status_code=400, detail="Item with such name already registered")
-    return crud.create_item(db=db, item=item)
-
-
-@app.get("/items/{item_id}", response_model=schemas.Item)
-def read_item(item_id: int, db: Session = Depends(get_db)):
-    db_item = crud.get_item(db, item_id=item_id)
-    if db_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return db_item
-
-
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
-
-
-@app.delete("/items/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_item(item_id: int, db: Session = Depends(get_db)):
-    db_item = crud.get_item(db, item_id=item_id)
-    if db_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    crud.delete_item(db=db, item_id=item_id)
-
-
-@app.put("/items/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
-def update_item(item_id: int, item: schemas.ItemBase, db: Session = Depends(get_db)):
-    db_item = crud.get_item(db, item_id=item_id)
-    if db_item is None:
-        raise HTTPException(status_code=404, detail="Item not found")
-    crud.update_item(db=db, item_id=item_id, item=item)
+app.include_router(router)
